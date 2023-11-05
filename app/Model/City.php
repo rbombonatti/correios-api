d977d55e4cff2e2d77c002a80c1c79442117b7a4
Rafael Bombonatti <rafael@we3online.com.br>
2023-11-05 20:22:02 -0300

    Crud Cidades

---
+++ app/Model/City.php
@@ -0,0 +1,72 @@
+<?php 
+
+namespace App\Model;
+
+use App\Utils\Sanitize;
+
+class City {
+
+    private $conn;
+    private $table_name = 'city';
+
+    public $city_id;
+    public $city_country;
+    public $city_name;
+    public $city_code;
+
+    public function __construct() {
+        $database = new Database();
+        $this->conn = $database->getConnection();
+    }
+
+    public function create($cities) {
+
+        try {
+            $query = "INSERT INTO $this->table_name 
+                (city_country, 
+                city_name, 
+                city_code) 
+                VALUES ";
+                
+            foreach ($cities as $key => $city) {
+                $city_country = Sanitize::clearString($city['sgPais']);
+                $city_name = Sanitize::clearString($city['noCidade']);
+                $city_code = Sanitize::clearString($city['coCidade']);
+
+                $query .= "('$city_country', 
+                    '$city_name',
+                    '$city_code'),";
+            }
+
+            $query = substr($query, 0, -1);
+            $stmt = $this->conn->prepare($query);
+
+            if ($stmt->execute()) {
+                return number_format($stmt->rowCount(), 0, ',', '.') . ' cidades inseridas!';
+            }
+
+        } catch (\PDOException $e) {
+            return "Erro: " . $e->getMessage();
+        }
+    }
+
+    public function readCities($sgPais) {
+        try {
+            $query = "
+                SELECT 
+                    DISTINCT
+                    city_name
+                FROM $this->table_name
+                WHERE
+                    city_country = '$sgPais'
+                ";
+            $stmt = $this->conn->prepare($query);
+            $stmt->execute();
+            return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+
+        } catch (\PDOException $e) {
+            return "Erro: " . $e->getMessage();
+        }
+    }
+
+}
