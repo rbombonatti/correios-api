4fe18d0fd33b959c5785c6344cc2181cb13dd3ed
Rafael Bombonatti <rafael@we3online.com.br>
2023-11-05 20:22:21 -0300

    Utils Sanitize

---
+++ app/Utils/Sanitize.php
@@ -0,0 +1,16 @@
+<?php 
+
+namespace App\Utils;
+
+
+class Sanitize {
+
+    public static function clearString($original)
+    {
+        $sanitized = htmlspecialchars(strip_tags($original));
+        $sanitized = str_replace("'", "", $sanitized);
+
+        return $sanitized;
+    }
+
+}
